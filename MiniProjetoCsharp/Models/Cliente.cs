f13e249fdc85ab8f44f36945f7452d1da4a16a16
Raul Vitor Melo Da Silva <raul.melo.da.silva@avanade.com>
2025-07-11 10:53:31 -0300

    teste commit cliente

---
+++ MiniProjetoCsharp/Models/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniProjetoCsharp.Models
+{
+    public class Cliente
+    {
+
+        [Key]
+        public int ClienteId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Endereco { get; set; }
+
+        public ICollection<Pedido> Pedidos { get; set; }
+    }
+}
